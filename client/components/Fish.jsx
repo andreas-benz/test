923220572905ab080db22220087b56fbd768c733
student <student@eda-bc-10.localdomain>
2022-01-28 10:27:38 +1300

    in process of making form

---
+++ client/components/Fish.jsx
@@ -0,0 +1,7 @@
+import React, { useState } from 'react'
+
+function Fish() {
+    
+}
+
+export default Fish
--- client/components/Home.jsx
+++ client/components/Home.jsx
@@ -1,8 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function Home () {  
+  const [formData, setFormData] = useState({
+    fish: ''
+  })
+
+  const handleChange = (e) => {
+    console.log(e.target.value)
+    setFormData(e.target.value)
+  }
+
+
   return (
-    <h1>React development has begun!</h1>
+  <>
+  <img src="images/fish_1.png" className="fish" id="fish1" width="200"/>
+  {/* <img src="images/fish_2.png" className="fish" alt="fish2" width="200"/>
+  <img src="images/fish_3.png" className="fish" alt="fish3" width="200"/> */}
+  <form >
+    <label htmlFor = 'fishList'>Fish:</label>
+    <input id='fishList' name='fishNumber' type='number' value={formData} onChange ={handleChange}/>
+  </form>
+
+
+  </>
+    
 
   )
 }
