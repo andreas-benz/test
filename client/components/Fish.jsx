7975e291838a650cf45eb8c13b300675216b625b
student <student@eda-bc-10.localdomain>
2022-01-28 12:47:11 +1300

    MVP done thank you guys

--- client/components/Fish.jsx
+++ client/components/Fish.jsx
@@ -1,10 +1,24 @@
 import React, { useState } from 'react'
 
+
 function Fish() {
-    
-      return (
+
+const randomDuration = () => `${Math.floor(Math.random() * 20) + 3}s`
+
+const randomBottom = () => `${Math.floor(Math.random() * 70) + 10}%`
+
+const randomDelay= () => `${(Math.floor(Math.random() * 40) + 1) * -1}s`
+
+
+    const style = {
+        animationDuration: randomDuration(),
+        bottom: randomBottom(),
+        animationDelay: randomDelay(),
+    }
+
+    return (
       <>
-      <img src="images/fish_1.png" className="fish" id="fish1" width="200"/>
+      <img style = {style} src="images/fish_1.png" className="fish" id="fish1" width="200"/>
       {/* <img src="images/fish_2.png" className="fish" alt="fish2" width="200"/>
       <img src="images/fish_3.png" className="fish" alt="fish3" width="200"/> */}
      
